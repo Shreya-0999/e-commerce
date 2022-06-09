95da9ba7eec79e212a124a9552f5cb98d10232ea
Shreya-0999 <shreya21999@gmail.com>
2022-06-09 14:01:49 +0530

    order history ui updated

--- src/Pages/OrderHistory/Styles/useStyles.js
+++ src/Pages/OrderHistory/Styles/useStyles.js
@@ -1,9 +1,15 @@
 import { makeStyles } from '@mui/styles';
+
 const useStyles = makeStyles((theme) => {
     return {
         container: {
-            width: '70%',
+            width: '65%',
             margin: '1rem auto',
+        },
+        pageHeading:{
+            marginTop:'3rem',
+            marginBottom: '1rem',
+            color:'#525252',
         },
         orderBox:{
             // padding: '1rem',
@@ -23,21 +29,55 @@ const useStyles = makeStyles((theme) => {
             minHeight:'14rem',
             padding: '1rem',
             borderBottom: '1px solid #c6c6c6',
+            
             // marginRight: '1rem',
         },
         itemDetails:{
-            margin: '6px',
-            fontSize: '14px !important',
-            
+            color:'#604f4f',
+            fontFamily: 'Nunito, sans-serif',
+            margin:'0',
+            fontSize: '15px !important',
+            lineHeight: '1.4 !important',
+            // letterSpacing: '1.2px !important',
+        },
+        orderHeading:{
+            fontFamily: 'Nunito, sans-serif',
+            fontSize: '20px',
+            color: '#8e7f7f',
+            letterSpacing: '1.5px',
+            marginBottom: '0.5rem',
+            fontWeight: '700',
+        },
+        productName:{
+            fontFamily: 'Nunito, sans-serif',
+            fontSize: '22px !important',
+            color: '#545151',
+            letterSpacing: '1px',
+            marginTop: '0',
+
         },
         priceSection:{
             borderRadius: '5px',
+            padding: '1rem',
             backgroundColor: '#f5f5f5',
         },
         img:{
             width: '90%',
             height: '95%',
-            borderRadius:'10px'
+            borderRadius:'8px'
+        },
+        bold:{
+            fontWeight: 'bold !important',
+            marginBottom: '0.5rem'
+        },
+        underline:{
+            width: '100px',
+            borderTop:'2px solid grey',
+            marginBottom:'3rem'
+        },
+        leftPadding:{
+            paddingLeft: '2.5rem !important',
+            paddingTop: '1rem !important',
         }
         
     }
--- src/Pages/OrderHistory/index.js
+++ src/Pages/OrderHistory/index.js
@@ -14,29 +14,29 @@ const OrderHistory = (props) => {
             {console.log(props.orderList)}
             <NavBar />
             <div className={classes.container}>
-                {/* <Stack className={classes.stack}> */}
-                <h1>My Orders</h1>
+                <h1 className={classes.pageHeading}>My Orders</h1>
+                <p className={classes.underline}></p>
                 {
                     props.orderList.map((ele, key) => (
                         <Grid container className={classes.orderBox}>
                             <Grid item xs={12}>
-                                <Typography className={classes.itemDetails}>#Order ID: {ele.orderId}</Typography>
+                                <Typography className={`${classes.itemDetails} ${classes.bold}`}>#Order ID: {ele.orderId}</Typography>
                             </Grid>
                             <Grid item xs container className={classes.orderContainer}>
-                                <Grid item xs={8} md={8}>
+                                <Grid item xs={12} md={8}>
                                     {
                                         ele.orderItems.map((item, key) => (
-                                            <Grid item xs={12} container className={classes.itemContainer}>
+                                            <Grid item xs={12} container className={classes.itemContainer} key={key}>
                                                 <Grid item xs={12} md={4}>
                                                     <ButtonBase sx={{ width: 200, height: 230 }}>
                                                         <img alt="complex" src={item.image} className={classes.img}/>
                                                     </ButtonBase>
                                                 </Grid>
-                                                <Grid item xs={12} md={8}>
-                                                    <Typography className={classes.itemDetails}>{item.name}</Typography>
-                                                    <Typography className={classes.itemDetails}>Qty: {item.quantity}</Typography>
-                                                    <Typography className={classes.itemDetails}>Size: {item.size}</Typography>
-                                                    <Typography className={classes.itemDetails}>Price: {item.price}</Typography>
+                                                <Grid item xs={12} md={8} className = {classes.leftPadding}>
+                                                    <Typography variant="h5" className={classes.productName}>{item.name}</Typography>
+                                                    <Typography variant="body1" className={classes.itemDetails}>Qty: {item.quantity}</Typography>
+                                                    <Typography variant="body1" className={classes.itemDetails}>Size: {item.size}</Typography>
+                                                    <Typography variant="body1" className={classes.itemDetails}>Price: Rs {item.price}</Typography>
                                                 </Grid>
                                             </Grid>
                                         ))
@@ -45,7 +45,15 @@ const OrderHistory = (props) => {
 
 
                                 <Grid item xs={4} md={4} container className={classes.priceSection}>
-                                    <h5>Hello Name</h5>
+                                    <Grid item xs={12}>
+                                        <h2 className={classes.orderHeading}>Shipping Details</h2>
+                                        <p className={`${classes.itemDetails} ${classes.bold}`}>{ele.address.FullName}, {ele.address.MobileNo},</p>
+                                        <p className={classes.itemDetails}>{ele.address.Locality}, {ele.address.Pincode}</p>
+                                        <p variant="body1" className={classes.itemDetails}>{ele.address.State},</p>
+                                        <p variant="body1" className={classes.itemDetails}>{ele.date}</p>
+                                        <h2 className={classes.orderHeading}>Payment Summary</h2>
+                                        <Typography variant="subtitle1" className={`${classes.itemDetails} ${classes.bold}`}>Amount Paid: Rs {ele.totalPrice}</Typography>
+                                    </Grid>
                                 </Grid>
 
                             </Grid>
